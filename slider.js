bce11f7eac930f0e9e8ac85361ebc508f69d98ef
j-o-the-wisp <j-o-the-wisp@nitemare>
2014-10-19 18:46:15 +0300

    left-right position changing without .clone()

--- slider.js
+++ slider.js
@@ -1,5 +1,13 @@
 $(document).ready(function () {
 
+    $(".arrow.left").click(function() {
+        changePositionLeft();
+    });
+
+    $(".arrow.right").click(function() {
+        changePositionRight();
+    });
+    
     function changePositionLeft() {
 
         var slides = $('.slides');
@@ -15,8 +23,20 @@ $(document).ready(function () {
 
         slides.children().first().appendTo(slides);
     }
+    function changePositionRight() {
 
-    $(".arrow.left").click(function() {
-        changePositionLeft();
-    });
+        var slides = $('.slides');
+
+        slides.children().each( function(index, child) {
+            if (index == 5) {
+                $(child).animate(slides.children().first().position());
+            }
+            else {
+                $(child).animate(slides.children().eq(index + 1).position());
+            }
+        });
+
+        slides.children().last().appendTo(slides);
+    }
+
 });
